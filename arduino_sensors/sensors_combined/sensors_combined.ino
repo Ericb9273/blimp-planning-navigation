018755fbce4a855b8a00cead6a2dcf950c0e3625
ericbi <ericbi@umich.edu>
2024-10-14 11:53:22 -0400

    10Hz output with specified data format

--- arduino_sensors/sensors_combined/sensors_combined.ino
+++ arduino_sensors/sensors_combined/sensors_combined.ino
@@ -8,6 +8,81 @@
 
 
 
+
+//IR Code
+
+// For TeraRanger One
+// #define SENSOR_ADDR 0x30
+
+// Create a Cyclic Redundancy Checks table used in the "crc8" function
+static const uint8_t crc_table[] = {
+    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31,
+    0x24, 0x23, 0x2a, 0x2d, 0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65,
+    0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d, 0xe0, 0xe7, 0xee, 0xe9,
+    0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
+    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1,
+    0xb4, 0xb3, 0xba, 0xbd, 0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2,
+    0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea, 0xb7, 0xb0, 0xb9, 0xbe,
+    0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
+    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16,
+    0x03, 0x04, 0x0d, 0x0a, 0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42,
+    0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a, 0x89, 0x8e, 0x87, 0x80,
+    0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
+    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8,
+    0xdd, 0xda, 0xd3, 0xd4, 0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c,
+    0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44, 0x19, 0x1e, 0x17, 0x10,
+    0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
+    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f,
+    0x6a, 0x6d, 0x64, 0x63, 0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b,
+    0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13, 0xae, 0xa9, 0xa0, 0xa7,
+    0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
+    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef,
+    0xfa, 0xfd, 0xf4, 0xf3
+};
+
+/*
+ * Brief : Calculate a Cyclic Redundancy Checks of 8 bits
+ * Param1 : (*p) pointer to receive buffer
+ * Param2 : (len) number of bytes returned by the TeraRanger
+ * Return : (crc & 0xFF) checksum calculated locally
+ */
+uint8_t crc8(uint8_t *p, uint8_t len) {
+  uint8_t i;
+  uint8_t crc = 0x0;
+  while (len--) {
+    i = (crc ^ *p++) & 0xFF;
+    crc = (crc_table[i] ^ (crc << 8)) & 0xFF;
+  }
+  return crc & 0xFF;
+}
+
+uint8_t buf[3];           // The variable "buf[3]" will contain the frame sent by the TeraRanger
+uint16_t IRdistance = 0;    // The variable "distance" will contain the distance value in millimeter
+uint8_t CRC = 0;          // The variable "CRC" will contain the checksum to compare at TeraRanger's one
+double IR_distance = 0;
+
+void IR_getvalue(){
+  Wire.write(0x00);  
+  Wire.requestFrom(IR_ADDRESS, 3);          // Read back three bytes from TR1 (THIS IS THE I2C BASE ADDRESS, CHANGE HERE IN CASE IT IS DIFFERENT)
+  buf[0] = Wire.read();               // First byte of distance
+  buf[1] = Wire.read();               // Second byte of distance
+  buf[2] = Wire.read();               // Byte of checksum
+  
+  CRC = crc8(buf, 2);                 // Save the "return" checksum in variable "CRC" to compare with the one sent by the TeraRanger
+  
+  if (CRC == buf[2]) {                 // If the function crc8 return the same checksum than the TeraRanger, then:
+    IRdistance = (buf[0]<<8) + buf[1];    // Calculate distance in mm                   // Start of the ERASABLE part                             //                                                   //
+  }
+  else {                                                    //
+    Serial.println("CRC error!"); }
+  IR_distance = (double)IRdistance / 1000;
+  
+  }
+
+
+
+  
+
 // Lidar code
 #include <Wire.h>     // Arduino standard I2C/Two-Wire Library
 #include "printf.h"
@@ -122,16 +197,18 @@ void barometer_setup()
 double init_altitude = 0;
 double base_distance = 0;
 void initialize_altitude() {
+  bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  delay(200);
+  bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  
   for(int i=0; i < 20; ++i ){
     init_altitude = init_altitude + bmp.readAltitude(SEALEVELPRESSURE_HPA);
-    tfmP.getData( tfDist, tfFlux, tfTemp);
-    base_distance = base_distance + tfDist;
     delay(100);
   }
-  init_altitude = init_altitude / 20;
-  //Serial.println(init_altitude);
-  base_distance = base_distance / 20;
-  //Serial.println(base_distance);
+  init_altitude = init_altitude / 20 - IR_distance;
+  Serial.println(IR_distance);
+  Serial.println(init_altitude);
+
 }
 
 bool whether_init_altitude = false;
@@ -142,10 +219,11 @@ void barometer_getvalue()
     delay(200);
     initialize_altitude();
     whether_init_altitude = true;
-    init_altitude = 200;
   }
-  //Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  baro_altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA) - init_altitude + base_distance;
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print(" ");
+  baro_altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA) - init_altitude ;
+  //Serial.println(baro_altitude);
 }
 
 
@@ -175,6 +253,7 @@ void IMU_setup() {
    myIMU.enableAcc(true);
 
   myIMU.setGyrRange(ICM20948_GYRO_RANGE_250);
+  myIMU.setAccRange(ICM20948_ACC_RANGE_2G);
 
   /*  Choose a level for the Digital Low Pass Filter or switch it off. 
    *  ICM20948_DLPF_0, ICM20948_DLPF_2, ...... ICM20948_DLPF_7, ICM20948_DLPF_OFF 
@@ -228,94 +307,18 @@ void IMU_getvalue(){
       float dtheta[3] = {omega[0] * dt_s, omega[1] * dt_s, omega[2] * dt_s};
       orientation = orientation * Quaternion::from_rotvec(dtheta);
       Quaternion::to_euler(orientation, pitch, roll, yaw);
-
-      //Serial.print("orientation: ");
-      //Serial.print(orientation.w);
-      //Serial.print(" ");
-      //Serial.print(orientation.x);
-      //Serial.print(" ");
-      //Serial.print(orientation.y);
-      //Serial.print(" ");
-      //Serial.println(orientation.z);
-
-      //Serial.print("Yaw: ");
-      //Serial.println(yaw);
-      //Serial.print(" Pitch: ");
-      //Serial.println(pitch);
-      //Serial.print(" Roll: ");
-      //Serial.println(roll);
-  }
-
-
-
-//IR Code
-
-// For TeraRanger One
-// #define SENSOR_ADDR 0x30
-
-// Create a Cyclic Redundancy Checks table used in the "crc8" function
-static const uint8_t crc_table[] = {
-    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31,
-    0x24, 0x23, 0x2a, 0x2d, 0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65,
-    0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d, 0xe0, 0xe7, 0xee, 0xe9,
-    0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
-    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1,
-    0xb4, 0xb3, 0xba, 0xbd, 0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2,
-    0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea, 0xb7, 0xb0, 0xb9, 0xbe,
-    0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
-    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16,
-    0x03, 0x04, 0x0d, 0x0a, 0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42,
-    0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a, 0x89, 0x8e, 0x87, 0x80,
-    0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
-    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8,
-    0xdd, 0xda, 0xd3, 0xd4, 0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c,
-    0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44, 0x19, 0x1e, 0x17, 0x10,
-    0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
-    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f,
-    0x6a, 0x6d, 0x64, 0x63, 0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b,
-    0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13, 0xae, 0xa9, 0xa0, 0xa7,
-    0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
-    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef,
-    0xfa, 0xfd, 0xf4, 0xf3
-};
-
-/*
- * Brief : Calculate a Cyclic Redundancy Checks of 8 bits
- * Param1 : (*p) pointer to receive buffer
- * Param2 : (len) number of bytes returned by the TeraRanger
- * Return : (crc & 0xFF) checksum calculated locally
- */
-uint8_t crc8(uint8_t *p, uint8_t len) {
-  uint8_t i;
-  uint8_t crc = 0x0;
-  while (len--) {
-    i = (crc ^ *p++) & 0xFF;
-    crc = (crc_table[i] ^ (crc << 8)) & 0xFF;
-  }
-  return crc & 0xFF;
-}
-
-uint8_t buf[3];           // The variable "buf[3]" will contain the frame sent by the TeraRanger
-uint16_t IRdistance = 0;    // The variable "distance" will contain the distance value in millimeter
-uint8_t CRC = 0;          // The variable "CRC" will contain the checksum to compare at TeraRanger's one
-double IR_distance = 0;
-
-void IR_getvalue(){
-  Wire.write(0x00);  
-  Wire.requestFrom(IR_ADDRESS, 3);          // Read back three bytes from TR1 (THIS IS THE I2C BASE ADDRESS, CHANGE HERE IN CASE IT IS DIFFERENT)
-  buf[0] = Wire.read();               // First byte of distance
-  buf[1] = Wire.read();               // Second byte of distance
-  buf[2] = Wire.read();               // Byte of checksum
-  
-  CRC = crc8(buf, 2);                 // Save the "return" checksum in variable "CRC" to compare with the one sent by the TeraRanger
-  
-  if (CRC == buf[2]) {                 // If the function crc8 return the same checksum than the TeraRanger, then:
-    IRdistance = (buf[0]<<8) + buf[1];    // Calculate distance in mm                   // Start of the ERASABLE part                             //                                                   //
-  }
-  else {                                                    //
-    Serial.println("CRC error!"); }
-  IR_distance = (double)IRdistance / 1000;
-  
+      Serial.print(corrAccRaw.x/ 16384.0);
+      Serial.print(" ");
+      Serial.print(corrAccRaw.y/ 16384.0);
+      Serial.print(" ");
+      Serial.print(corrAccRaw.z/ 16384.0);
+      Serial.print(" ");
+      Serial.print(gyr.x);
+      Serial.print(" ");
+      Serial.print(gyr.y);
+      Serial.print(" ");
+      Serial.print(gyr.z);
+      Serial.print(" ");
   }
 
 
@@ -327,7 +330,7 @@ double P_last = 1; // 初始估计协方差，设为非零值
 double Q = 0.022; // 过程噪声协方差
 double R = 0.617; // 测量噪声协方差
 
-double kalmanfilter(double h1, double h2) {
+double kalmanfilter(double h1, double h2, double weight_h1=0.2, double weight_h2=0.8) {
   double x_pred = x_est_last;
   double P_pred = P_last + Q;
 
@@ -337,8 +340,6 @@ double kalmanfilter(double h1, double h2) {
   double P_est = (1 - K) * P_pred;
 
   // 使用h2更新，调整权重
-  double weight_h1 = 0.3; // h1的权重
-  double weight_h2 = 0.7; // h2的权重
   K = P_est / (P_est + R);
   x_est = x_est + K * (weight_h1 * h1 + weight_h2 * h2 - x_est);
   P_est = (1 - K) * P_est;
@@ -364,37 +365,39 @@ void setup() {
   barometer_setup();
 }
 
+
 void loop() {
-  Serial.println("------------------------------");
-  
-  // Barometer
-  Wire.beginTransmission(BAROMETER_ADDRESS);
-  barometer_getvalue();
+  //Serial.println("------------------------------");
+  // IR
+  Wire.beginTransmission(IR_ADDRESS);
+  IR_getvalue();
   Wire.endTransmission();
   
   // LIDAR
-  Wire.beginTransmission(LIDAR_ADDRESS);
-  Lidar_getvalue();
-  Wire.endTransmission();
+//  Wire.beginTransmission(LIDAR_ADDRESS);
+//  Lidar_getvalue();
+//  Wire.endTransmission();
   
   // IMU
   Wire.beginTransmission(IMU_ADDRESS);
   IMU_getvalue();
   Wire.endTransmission();
 
-  // IR
-  Wire.beginTransmission(IR_ADDRESS);
-  IR_getvalue();
+  // Barometer
+  Wire.beginTransmission(BAROMETER_ADDRESS);
+  barometer_getvalue();
   Wire.endTransmission();
 
+ Serial.println(IR_distance);
+
+ 
   double lidar_Altitude = lidar_distance * cos(pitch)* cos(roll);
   double IR_Altitude = IR_distance * cos(pitch)* cos(roll);
-  Serial.print("lidar_Altitude:");
-  Serial.print(lidar_Altitude);
-  Serial.print("\t");
-  Serial.print("IR_Altitude:");
-  Serial.println(IR_Altitude);
-  //Serial.println(baro_altitude);
-  //kalmanfilter(Altitude,  baro_altitude);
-  delay(200);
+//  Serial.print("lidar_Altitude:");
+//  Serial.print(lidar_Altitude);
+//  Serial.print("\t");
+// Serial.print("IR_Altitude:");
+// Serial.println(IR_Altitude);
+// Serial.println(baro_altitude);
+// kalmanfilter(IR_Altitude,  baro_altitude);
 }
